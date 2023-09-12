317d6f8902778c57df68b8cbb1356046bd4cf647
Johnyong <johnyong@MacBook-Pro-khxng-Johnyong.local>
2023-09-12 12:18:19 +0700

    add index.js and array.js

---
+++ array.js
@@ -0,0 +1,15 @@
+const myFood = ["Sushi", "Fried Chicken", "Steak", "Shabu"];
+
+//1
+console.log(myFood.indexOf("Steak"));
+
+//2
+myFood.push("Yakiniku", "Dessert", "Pasta");
+
+console.log(myFood);
+
+//3
+
+console.log(myFood.includes("Steak"));
+
+//test
